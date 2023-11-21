090dfc7a81432497d40a33463af4a6c9789f23df
ngoquangtu <ngoquangtuhd@gmail.com>
2023-11-21 21:09:05 +0700

    up game test

---
+++ Assets/Scripts/Core/FPSController.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSController : PortalTraveller {
+
+    [SerializeField] private float walkSpeed = 3;
+    [SerializeField] private float runSpeed = 6;
+    [SerializeField] private float smoothMoveTime = 0.1f;
+    [SerializeField] private float jumpForce = 8;
+    [SerializeField] private float gravity = 18;
+
+    [SerializeField] private bool lockCursor=true;
+    [SerializeField] private float mouseSensitivity = 10;
+    [SerializeField] private Vector2 pitchMinMax = new Vector2 (-40, 85);
+    [SerializeField] private float rotationSmoothTime = 0.1f;
+
+    CharacterController controller;
+    Camera cam;
+    public float yaw;
+    public float pitch;
+    float smoothYaw;
+    float smoothPitch;
+
+    float yawSmoothV;
+    float pitchSmoothV;
+    float verticalVelocity;
+    Vector3 velocity;
+    Vector3 smoothV;
+    Vector3 rotationSmoothVelocity;
+    Vector3 currentRotation;
+
+    bool jumping;
+    float lastGroundedTime;
+    bool disabled;
+
+    private void Start () {
+        cam = Camera.main;
+        if (lockCursor) {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        controller = GetComponent<CharacterController> ();
+
+        yaw = transform.eulerAngles.y;
+        pitch = cam.transform.localEulerAngles.x;
+        smoothYaw = yaw;
+        smoothPitch = pitch;
+    }
+
+    private void Update () {
+        Moving();
+        MouseControl();
+    }
+    private void MouseControl()
+    {
+        float mX = Input.GetAxis ("Mouse X");
+        float mY = Input.GetAxis ("Mouse Y");
+        float mMag = Mathf.Sqrt (mX * mX + mY * mY);
+        if (mMag > 5) {
+            mX = 0;
+            mY = 0;
+        }
+
+        yaw += mX * mouseSensitivity;
+        pitch -= mY * mouseSensitivity;
+        pitch = Mathf.Clamp (pitch, pitchMinMax.x, pitchMinMax.y);
+        smoothPitch = Mathf.SmoothDampAngle (smoothPitch, pitch, ref pitchSmoothV, rotationSmoothTime);
+        smoothYaw = Mathf.SmoothDampAngle (smoothYaw, yaw, ref yawSmoothV, rotationSmoothTime);
+
+        transform.eulerAngles = Vector3.up * smoothYaw;
+        cam.transform.localEulerAngles = Vector3.right * smoothPitch;
+    }
+    private void Moving()
+    {
+        Vector2 input = new Vector2 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"));
+
+        Vector3 inputDir = new Vector3 (input.x, 0, input.y).normalized;
+        Vector3 worldInputDir = transform.TransformDirection (inputDir);
+
+        float currentSpeed = (Input.GetKey (KeyCode.LeftShift)) ? runSpeed : walkSpeed;
+        Vector3 targetVelocity = worldInputDir * currentSpeed;
+        velocity = Vector3.SmoothDamp (velocity, targetVelocity, ref smoothV, smoothMoveTime);
+
+        verticalVelocity -= gravity * Time.deltaTime;
+        velocity = new Vector3 (velocity.x, verticalVelocity, velocity.z);
+
+        var flags = controller.Move (velocity * Time.deltaTime);
+        if (flags == CollisionFlags.Below) {
+            jumping = false;
+            lastGroundedTime = Time.time;
+            verticalVelocity = 0;
+        }
+
+        if (Input.GetKeyDown (KeyCode.Space)) {
+            float timeSinceLastTouchedGround = Time.time - lastGroundedTime;
+            if (controller.isGrounded || (!jumping && timeSinceLastTouchedGround < 0.15f)) {
+                jumping = true;
+                verticalVelocity = jumpForce;
+            }
+        }
+    }
+
+    public override void Teleport (Transform fromPortal, Transform toPortal, Vector3 pos, Quaternion rot) {
+        transform.position = pos;
+        Vector3 eulerRot = rot.eulerAngles;
+        float delta = Mathf.DeltaAngle (smoothYaw, eulerRot.y);
+        yaw += delta;
+        smoothYaw += delta;
+        transform.eulerAngles = Vector3.up * smoothYaw;
+        velocity = toPortal.TransformVector (fromPortal.InverseTransformVector (velocity));
+        Physics.SyncTransforms ();
+    }
+
+}
